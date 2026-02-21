86bab28aa0ffdfd3bf395fe481f3122832a8d1d7
Alexander Dyankov <alexander.dyankov@gmail.com>
2026-02-21 23:08:35 +0200

    Added methods to session controller

--- Backend/GrizzlyHackBackend/src/main/java/com/mischievous/fairies/controller/SessionController.java
+++ Backend/GrizzlyHackBackend/src/main/java/com/mischievous/fairies/controller/SessionController.java
@@ -1,5 +1,9 @@
 package com.mischievous.fairies.controller;
 
+import com.mischievous.fairies.service.JwtService;
+import com.mischievous.fairies.service.SessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,8 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/sessions")
 public class SessionController {
-    @PostMapping
-    public ResponseEntity<Void> createSession(@CookieValue(name = "userToken")) {
-        
+
+    private final SessionService sessionService;
+    private final JwtService jwtService;
+    @Autowired
+    public SessionController(SessionService sessionService, JwtService jwtService) {
+        this.sessionService = sessionService;
+        this.jwtService = jwtService;
+    }
+    @PostMapping("/start")
+    public ResponseEntity<Void> createSession(@CookieValue(name = "userToken") String userToken) {
+        sessionService.startSession(jwtService.extractUserData(userToken).getId());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/end")
+    public ResponseEntity<Void> endSession(@CookieValue(name = "userToken") String userToken) {
+        sessionService.endSession(jwtService.extractUserData(userToken).getId());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- Backend/GrizzlyHackBackend/src/main/java/com/mischievous/fairies/repository/SessionRepository.java
+++ Backend/GrizzlyHackBackend/src/main/java/com/mischievous/fairies/repository/SessionRepository.java
@@ -1,9 +1,10 @@
 package com.mischievous.fairies.repository;
 
-import com.mischievous.fairies.model.entity.UserEntity;
+import com.mischievous.fairies.model.entity.SessionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity,Long> {
+public interface SessionRepository extends JpaRepository<SessionEntity, Long> {
+
 }
