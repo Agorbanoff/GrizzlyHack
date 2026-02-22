fb84b66fd267a86d3489b3d5f24799fd39fb9b70
Alexander Dyankov <alexander.dyankov@gmail.com>
2026-02-22 10:22:56 +0200

    Added a missing annotation

--- Backend/GrizzlyHackBackend/src/main/java/com/mischievous/fairies/model/dto/res/ScreenshotResDto.java
+++ Backend/GrizzlyHackBackend/src/main/java/com/mischievous/fairies/model/dto/res/ScreenshotResDto.java
@@ -2,9 +2,11 @@ package com.mischievous.fairies.model.dto.res;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ScreenshotResDto {
     private Long id;
 }
